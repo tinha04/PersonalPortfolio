06c54b8732cce4af69e4eb25dbb4ed7bfa95d6c1
tinha04 <130317922+tinha04@users.noreply.github.com>
2024-10-03 03:14:31 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Navbar from './components/NavBar/Navbar';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <Home />
+      <About />
+  </main>
+
+  );
+}
+
+export default App;
+
+
